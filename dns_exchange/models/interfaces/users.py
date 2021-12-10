80f5e092c38b1a277839b85e8ab63ee7ca4c0716
Ilnar Gomelyanov <hibushland@gmail.com>
2021-12-11 02:20:38 +0300

    add generate address and seep phrase

--- dns_exchange/models/interfaces/users.py
+++ dns_exchange/models/interfaces/users.py
@@ -1,6 +1,23 @@
 from abc import ABC, abstractmethod
+from hashlib import sha512
+from random import randrange, sample
 
 from dns_exchange.models.interfaces.common import BaseModelInterface, BaseDescriptorInterface
+
+import requests
+
+word_site = "https://www.mit.edu/~ecprice/wordlist.10000"
+response = requests.get(word_site)
+WORDS = response.content.splitlines()
+
+
+def get_address():
+    return str(hex(randrange(0, 4294967295)))
+
+
+def get_seed_phrase():
+    seed_phrase = ' '.join(x.decode('utf-8') for x in sample(WORDS, 8))
+    return sha512(str.encode(seed_phrase)).hexdigest()
 
 
 class UserAssetsInterface(BaseDescriptorInterface, ABC):
@@ -11,10 +28,10 @@ class UserInterface(BaseModelInterface, ABC):
     table_name = 'users'
     __slots__ = ('id', 'address', 'seed_phrase', 'is_admin', 'assets')
 
-    def __init__(self, address, seed_phrase):
+    def __init__(self):
         super().__init__(assets={})
-        self.address = address  # TODO: Generate address '0x9c3f7c50'
-        self.seed_phrase = seed_phrase  # TODO: Generate seed_phrase 'red blue green yellow orange dick'
+        self.address = get_address()
+        self.seed_phrase = get_seed_phrase()
         self.is_admin = False
         self.__class__.assets = self._get_assets_descriptor_obj()
 
