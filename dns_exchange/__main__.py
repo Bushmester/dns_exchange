8aad6b1a57f8b5c603586e41c7556bc19a6d863a
Ramil Suleymanov <lllumineux@outlook.com>
2021-12-12 03:18:48 +0300

    Delete tests from __main__.py

--- dns_exchange/__main__.py
+++ dns_exchange/__main__.py
@@ -8,7 +8,6 @@ from typing import Union
 from dns_exchange import config
 from dns_exchange.dictionaries import commands_dict
 from dns_exchange.helpers import Request, Response
-from dns_exchange.models.mongo.users import User
 from dns_exchange.registers import register_all_commands
 
 
@@ -71,7 +70,4 @@ def main():
 
 
 if __name__ == "__main__":
-    # main()
-
-    user = User.retrieve(id='61b50ea906561fa61f58ee8b')
-    print(dict(user.assets))
+    main()
