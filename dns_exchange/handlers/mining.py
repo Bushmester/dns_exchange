5e257f2e1a5a13ea8130c5680e73b71199f6f424
Ilnar Gomelyanov <hibushland@gmail.com>
2021-12-12 13:42:09 +0300

    add mine

--- dns_exchange/handlers/mining.py
+++ dns_exchange/handlers/mining.py
@@ -1,5 +1,12 @@
+from random import uniform, randrange
+
+from dns_exchange.dictionaries import auth_dict
 from dns_exchange.helpers import Response
 from dns_exchange.validators import Number
+
+
+TOKEN = uniform(0, 1000)
+NUMBER = randrange(0, 10)
 
 
 # mine command
@@ -11,7 +18,23 @@ class MineCommandData:
         self.number = kwargs['number']
 
 
-def mine(**kwargs):
+def mine(auth_token: str, **kwargs):
     data = MineCommandData(**kwargs)
     # TODO: Mine logic
-    return Response()
+    response = Response()
+
+    try:
+        user = auth_dict[auth_token]
+
+        if data.number == NUMBER:
+            response.add_content_text(
+                lines=[f"You've received {TOKEN} DNS tokens!"],
+            )
+        else:
+            response.add_content_text(
+                lines=["Unsuccessful mining!"],
+            )
+    except KeyError:
+        response.add_error("auth is required!")
+
+    return response
