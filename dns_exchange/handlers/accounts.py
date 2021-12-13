0122522a595920901ae8745c9fa01feade898c06
eale <novode25@gmail.com>
2021-12-13 03:43:56 +0300

    start adding test_tokens

--- dns_exchange/handlers/accounts.py
+++ dns_exchange/handlers/accounts.py
@@ -15,10 +15,9 @@ def generate_auth_token() -> str:
 
 # create_account command
 @auth_not_required
-def create_account() -> Response:
+def create_account():
     new_account = User.create()
     new_account.save()
-
     response = Response()
     response.add_content_text(
         title="New account has been successfully created!",
