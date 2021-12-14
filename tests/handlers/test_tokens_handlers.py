fbbb7e47e486244150002e6c680ec1e500be4a70
eale <novode25@gmail.com>
2021-12-14 04:08:37 +0300

    add failed buy test

--- tests/handlers/test_tokens_handlers.py
+++ tests/handlers/test_tokens_handlers.py
@@ -7,60 +7,67 @@ from dns_exchange.models.mongo.users import User
 from tests import helpers
 
 
-@pytest.mark.usefixtures("clean_db")
-def test_add_token():
-    user_info = helpers.get_user(is_admin=True, auth_token=True)
-    user = user_info['user']
-    auth_token = user_info['auth_token']
-    btc_count = 3.0
+# @pytest.mark.usefixtures("clean_db")
+# def test_add_token():
+#     user_info = helpers.get_user(is_admin=True, auth_token=True)
+#     user = user_info['user']
+#     auth_token = user_info['auth_token']
+#     btc_count = 3.0
+#
+#     response_from_add_token = tokens.add_token(auth_token=auth_token, tag='BTC', quantity=btc_count)
+#
+#     user = User.retrieve(address=user.address)
+#     btc_count_from_db = user.assets['BTC']
+#
+#     assert btc_count_from_db == btc_count
 
-    response_from_add_token = tokens.add_token(auth_token=auth_token, tag='BTC', quantity=btc_count)
 
-    user = User.retrieve(address=user.address)
-    btc_count_from_db = user.assets['BTC']
-
-    assert btc_count_from_db == btc_count
-
-
-@pytest.mark.usefixtures("clean_db")
 def test_buy():
-    user_info = helpers.get_user(auth_token=True, is_admin=True)
-    user = user_info['user']
-    auth_token = user_info['auth_token']
+    user_info_1 = helpers.get_user(auth_token=True, is_admin=True)
+    user_1 = user_info_1['user']
+    auth_token_1 = user_info_1['auth_token']
+
+    user_info_2 = helpers.get_user(auth_token=True, is_admin=True)
+    user_2 = user_info_2['user']
+    auth_token_2 = user_info_2['auth_token']
+
+    print(user_2.address, user_1.address)
 
     token1 = 'CAT'
     token2 = 'DOG'
     pair = f'{token1}_{token2}'
 
-    amount = 1.0
-    exchange_rate = 1.0
+    tokens.add_token(auth_token=auth_token_1, tag=token1, quantity=15.0)
+    tokens.add_token(auth_token=auth_token_2, tag=token2, quantity=10.0)
 
-    tokens.add_token(auth_token=auth_token, tag=token1, quantity=amount)
+    response_from_buy = tokens.buy(trading_pair=pair, amount=5.0,
+                                   exchange_rate=1.0, auth_token=auth_token_2)
+    response_from_sell = tokens.sell(trading_pair=pair, amount=10.0,
+                                     exchange_rate=1.1, auth_token=auth_token_1)
 
-    response_from_buy = tokens.buy(trading_pair=pair, amount=amount,
-                                   exchange_rate=exchange_rate, auth_token=auth_token)
     print(response_from_buy.get_json_string())
-
-    assert 1 == 1
-
-
-@pytest.mark.usefixtures("clean_db")
-def test_sell():
-    user_info = helpers.get_user(auth_token=True, is_admin=True)
-    user = user_info['user']
-    auth_token = user_info['auth_token']
-
-    token1 = 'WWW'
-    token2 = 'SSS'
-    pair = f'{token1}_{token2}'
-
-    amount = 1.0
-    exchange_rate = 1.0
-
-    tokens.add_token(auth_token=auth_token, tag=token1, quantity=amount)
-
-    response_from_sell = tokens.sell(trading_pair=pair, amount=amount,
-                                     exchange_rate=exchange_rate, auth_token=auth_token)
     print(response_from_sell.get_json_string())
 
     assert 1 == 1
+
+
+# @pytest.mark.usefixtures("clean_db")
+# def test_sell():
+#     user_info = helpers.get_user(auth_token=True, is_admin=True)
+#     user = user_info['user']
+#     auth_token = user_info['auth_token']
+#
+#     token1 = 'WWW'
+#     token2 = 'SSS'
+#     pair = f'{token1}_{token2}'
+#
+#     amount = 1.0
+#     exchange_rate = 1.0
+#
+#     tokens.add_token(auth_token=auth_token, tag=token1, quantity=amount)
+#
+#     response_from_sell = tokens.sell(trading_pair=pair, amount=amount,
+#                                      exchange_rate=exchange_rate, auth_token=auth_token)
+#     print(response_from_sell.get_json_string())
+#
+#     assert 1 == 1
--- tests/helpers.py
+++ tests/helpers.py
@@ -4,25 +4,10 @@ from dns_exchange.models.mongo.users import User
 from dns_exchange.handlers import accounts
 
 
-class CustomUser(User):
-
-    def __init__(self, obj_id, is_new, **kwargs):
-        super().__init__(obj_id, is_new, **kwargs)
-        self.auth_token = None
-
-    @property
-    def auth_token(self):
-        return self.auth_token
-
-    @auth_token.setter
-    def auth_token(self, value):
-        self.auth_token = value
-
-
 def get_user(is_admin=False, auth_token=False):
     result = {}
 
-    user = CustomUser.create(is_admin=is_admin)
+    user = User.create(is_admin=is_admin)
     user.save()
 
     result['user'] = user
