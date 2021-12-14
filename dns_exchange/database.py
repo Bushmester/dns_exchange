c3123586999a461c86053eb82e11a445860f4498
Ilnar Gomelyanov <hibushland@gmail.com>
2021-12-14 13:07:59 +0300

    add redis

--- dns_exchange/database.py
+++ dns_exchange/database.py
@@ -1,7 +1,9 @@
 from pymongo import MongoClient
+from redis import Redis
 from dns_exchange.config import MONGO_DB_LINK
-
 
 cluster = MONGO_DB_LINK
 client = MongoClient(cluster)
 db = client.dns
+
+redis = Redis()
